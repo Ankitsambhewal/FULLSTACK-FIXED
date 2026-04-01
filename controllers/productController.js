612e7559e307115c73bedb52229f08c8dfa20afd
Ankitsambhewal <ankitsambherwal134@gmail.com>
2026-04-01 12:11:40 +0530

    Fixing case sensitivity for Order model

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 
 // ================= GET PRODUCTS (Menu Page) =================
 exports.getProducts = async (req, res) => {
